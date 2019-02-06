3ccf938978f70c8d28a00d73a46576bc9fef71db
dishasahu <sahudisha07@gmail.com>
2019-02-06 09:08:19 +0530

    welcome message successfull added to the repository

---
+++ Module02/welcome.py
@@ -0,0 +1,17 @@
+Welcomemessage = "Good Morning! Welcome to My academy."
+Lastname = firstname = gender = name = waitmessage = ''
+
+# printing the welcome message
+
+print(Welcomemessage)
+
+#conversation
+
+Lastname=input("what is your lastname sir")
+Firstname=input("what is your Firstname sir")
+gender=input("may i know your gender please")
+name=input("Whome do you want to meet sir")
+
+#priting the message
+Waiting_message="Hello Mr.{} {},Mr. {} will meet you in few minutes .I request you to take a seat .".format(Fristname, Lastmane, name)
+print(Waiting_message)
